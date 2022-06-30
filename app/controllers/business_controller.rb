b72a919ada1d69a4592393da1f5f575616cb5bac
Deya Card <thedeyacard@gmail.com>
2022-06-30 13:55:28 -0700

    update business_controller file

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -5,9 +5,33 @@ class BusinessController < ApplicationController
     json_response(@business)
   end
 
+  def show
+    @business = Business.find(params[:id])
+    json_response(@business)
+  end
+
+  def create
+    @business = Business.create(business_params)
+    json_response(@business)
+  end
+
+  def update
+    @business = Business.find(params[:id])
+    @business.update(business_params)
+  end
+
+  def destroy
+    @business = Business.find(params[:id])
+    @business.destroy
+  end
+
   private
   def json_response(object, status = :ok)
     render json: object, status: status
+  end
+
+  def business_params
+    params.permit(:restaurant, :shop)
   end
 end
 
