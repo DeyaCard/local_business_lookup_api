f152fd8b637d50fef162012149b3126019642ec3
Deya Card <thedeyacard@gmail.com>
2022-07-02 23:37:10 -0700

    fix typo in business_spec

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -4,5 +4,5 @@ describe Business do
   it { should validate_presence_of :name }
   it { should validate_presence_of :location }
   it { should validate_presence_of :description }
-  it { should validate_presence_of :phone }
+  it { should validate_numericality_of(:phone).only_integer }
 end
