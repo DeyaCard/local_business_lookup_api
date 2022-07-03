a9688d77850ce53cc3e1e27a56a9ba7c5cac3c08
Deya Card <thedeyacard@gmail.com>
2022-07-02 23:29:32 -0700

    add patch_route_spec file

---
+++ spec/requests/patch_route_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "edit a business route", type: :request do
+  before do
+    patch '/businesses', params: { :restaurant => 'test_restaurant', :shop => 'test_shop' }
+
+    WIP
+
+    
+
+
+
